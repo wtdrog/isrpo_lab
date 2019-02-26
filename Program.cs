943d25f400597ca2d2c5796800e52c9ab40ccbaa
Vladislav <12v12v6@gmail.com>
2019-02-26 18:45:48 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace isrpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
