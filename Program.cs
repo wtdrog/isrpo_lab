d9ee7989d63f5f938fa73dfb6d6a51669d136bd5
Vladislav <12v12v6@gmail.com>
2019-03-12 20:30:00 +0300

    Отмечены места для частей программы

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ namespace isrpo
 {
     class Program
     {
+        // Реализация меню будет находиться здесь
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
+
+    // Реализация работника будет здесь
+
+    // Реализация фильтра будет здесь
 }
